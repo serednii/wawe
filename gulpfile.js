3aea90b1eeb4e3f751a9805fb1b30f363bcf0d15
serednii <seredniimykola@gmail.com>
2021-07-31 18:19:52 +0200

    wawe 1

---
+++ gulpfile.js
@@ -0,0 +1,93 @@
+const { src, dest, parallel, series } = require('gulp');
+var sass = require('gulp-sass')(require('sass'));
+var gulp = require('gulp');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+var  watch  = require('gulp-watch');
+var  uglify  = require('gulp-uglify');
+var  imagemin  = require('gulp-imagemin');
+const  del  = require('del');
+
+const sync = require('browser-sync').create();
+
+function styles(){
+    return src('app/scss/**/*.scss')
+   .pipe(sass ({outputStyle:'compressed'}).on('error', sass.logError))
+   .pipe(concat('style.min.css'))
+   .pipe(autoprefixer({
+       overrideBrowserslist:['last 10 versions'],
+       grid:true
+    }))
+    .pipe(dest('app/css'))
+    .pipe(sync.stream())
+}
+
+function browser(){
+  sync.init({
+    server:{
+      baseDir:'./'
+    },
+    notify: false
+  })
+}
+
+function scripts(){
+  return src(
+    [
+     // 'node_modules/jquery/dist/jquery.js',
+      'app/js/main.js'
+    ])  
+     .pipe(concat('main.min.js'))
+    .pipe(uglify()) // мініиізує файл
+     .pipe(dest('app/js'))
+     .pipe(sync.stream())
+}
+ 
+function images(){
+  return src('app/images/**/*.*')
+  .pipe(imagemin([
+    imagemin.gifsicle({interlaced:true}),
+    imagemin.mozjpeg({quality: 75, progressive: true}),
+    imagemin.optipng({optimizationLevel:5}),
+    imagemin.svgo({
+      plugins:[
+        {removeViewBox:true},
+        {cleanupIDs: false}
+      ]
+    })
+  ]))
+  .pipe(dest('dist/images'))
+}
+
+function build(){
+  return src([
+    'app/**/*.html',
+    'app/css/style.min.css',
+    'app/js/main.min.js'
+  ],{base: 'app'})
+  .pipe(dest('dist'))
+}
+
+function cleanDist(){
+  return del('dist')
+}
+
+function watching(){
+   //watch(['app/scss/**/*.scss'],styles);
+   //watch(['app/js**/*.js','!app/js/main.min.js'], scripts);
+   //watch(['app/**/*.html',]).on('change', sync.reload);
+    watch(['css/**/*.css'],styles);
+    watch(['js**/*.js'], scripts);
+   watch(['**/*.html',]).on('change', sync.reload);
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.browser = browser;
+exports.watching = watching;
+exports.images = images;
+exports.build = series(cleanDist, images, build);
+exports.cleanDist = cleanDist;
+//exports.default = parallel(styles, scripts, browser, watching);
+
+exports.default = parallel(browser, watching);
